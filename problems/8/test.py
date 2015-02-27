3ab1861a8b4fab7dd8619b5697a90c697feac8d0
tmtowtdi <tmtowtdi@gmail.com>
2015-02-27 18:11:59 -0500

    starting 8

--- problems/8/test.py
+++ problems/8/test.py
@@ -1,3 +1,19 @@
 
 # http://www.pythonchallenge.com/pc/def/integrity.html
 
+"""
+Picture of a bee, part of which is hyperlinked to ../return/good.html.
+
+That hyperlinked URL is password protected.  The realm is "inflate".
+
+
+This is in a comment in the page:
+<!--
+un: 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+pw: 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+-->
+
+Back to ZipFile, I assume.
+
+"""
+
