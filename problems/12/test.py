e7f1c301359b1b43c3b0355cec6e58d875016701
tmtowtdi <tmtowtdi@gmail.com>
2015-03-03 18:11:13 -0500

    12 not solved.  Done with this crap.

--- problems/12/test.py
+++ problems/12/test.py
@@ -1,3 +1,34 @@
 
 # http://www.pythonchallenge.com/pc/return/evil.html
 
+"""
+Page contains "evil1.jpg".
+
+Since it's numbered, try "evil2.jpg", which is an image whose text suggests we 
+try "evil2.gfx".
+
+evil2.gfx can't be displayed in the browser - downloaded.
+
+evil3.jpg says "no more evils"
+
+evil4.jpg exists, but isn't a jpg.  It's a text file containing
+    Bert is evil! go back!
+
+http://www.pythonchallenge.com/pc/return/bert.html
+    caption: "Yes! Bert is evil!"
+    image: Bert the muppet looking evil and possibly raping another muppet.
+    I think this is teh funnay and not part of the puzzle. 
+
+At this point, a solution site tells me the "bert is evil" think is a clue for 
+problem 13, and this problem appears to be more nonsense involving interleaved 
+graphics files.
+
+I'm tired of fucking with graphics files; I'm learning more about graphics 
+file formats than I am about Python, and the fact that the bert clue in this 
+puzzle is a hint for the next puzzle means that the whole thing is trying way 
+too hard to be mysterious and hard-to-solve for non-python reasons.  
+
+I think I'm done with this now.  Back to the much more reasonable Euler 
+project.
+"""
+
